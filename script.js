f554ef52a8527ce9b8f4d916a211a793ca51f1ec
Hlxuan <huanglangxuan@hlxuan.top>
2024-11-02 12:11:04 +0800

    update:动态设置进度条长度

--- script.js
+++ script.js
@@ -78,5 +78,6 @@ audio.addEventListener("timeupdate", updateProgress)
 function updateProgress(e) {
   const { duration, currentTime } = e.srcElement
   const progressPercent = (currentTime / duration) * 100
-  console.log(progressPercent)
+
+  progress.style.width = `${progressPercent}%`
 }
